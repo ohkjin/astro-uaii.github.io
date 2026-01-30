190f1b4b0caa246f8e74a56fc54d7524fdd84583
jihn <148052729+ohkjin@users.noreply.github.com>
2026-01-30 16:39:35 +0900

    redirectPath

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,14 @@ export default defineConfig({
     },
     // site: 'https://kaist-uaii.github.io',
     // base: 'kaist-uaii'
-     site: 'https://ohkjin.github.io',
-    base: 'astro-uaii.github.io'
+    site: 'https://ohkjin.github.io',
+    base: 'astro-uaii.github.io',
+    redirects: {
+   '/events': '/astro-uaii.github.io/events',
+   '/events-detail': '/astro-uaii.github.io/events-detail',
+   '/press-detail': '/astro-uaii.github.io/press-detail',
+   '/index': '/astro-uaii.github.io/',
+   '/events-detail/[...slug]': '/astro-uaii.github.io/events-detail/[...slug]',
+   '/press-detail/[...slug]': '/astro-uaii.github.io/press-detail/[...slug]',
+    }
 });
