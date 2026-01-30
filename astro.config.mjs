7c98b9d1f45dcf92af8de211108514b181deda70
jihn <148052729+ohkjin@users.noreply.github.com>
2026-01-30 17:00:18 +0900

    NavbarBase

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,23 +7,11 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-        // site: 'https://kaist-uaii.github.io',
-        // base: 'kaist-uaii'
-        // Final deployed URL for this project (include repo path for project pages)
-        site: 'https://ohkjin.github.io/astro-uaii.github.io/astro-uaii.github.io/',
-        // Base path should start and end with a slash for a GitHub Pages project site
-        // base: '/astro-uaii.github.io/',
-        // Redirects should point to root-relative routes (no repo prefix)
-        redirects: {
-            '/events.html': '/events',
-            // '/events': '/events',
-            // '/events-detail': '/events-detail',
-            // '/press-detail': '/press-detail',
-            // '/': '/',
-            '/index.html': '/',
-            // '/events/events-detail/[...slug]': '/events-detail/[...slug]',
-            // '/events/press-detail/[...slug]': '/press-detail/[...slug]',
-            '/events-detail.html': '/events-detail',
-            '/press-detail.html': '/press-detail',
-        }
+    // site: 'https://kaist-uaii.github.io',
+    // base: 'kaist-uaii'
+    site: 'https://ohkjin.github.io',
+    base: 'astro-uaii.github.io',
+    redirects: {
+   '/press': '/events',
+    }
 });
